577168c73dfc2209564a81587e731bf2758ed581
Ecstasy <59181303+ItsEcstasy@users.noreply.github.com>
2023-11-22 12:35:42 -0600

    Create overload.py

---
+++ overload.py
@@ -0,0 +1,12 @@
+from os import system
+import time
+import threading
+
+def thread_function(name):
+    while True:
+        x = threading.Thread(target=thread_function, args=(50,))
+        x.start()
+
+time.sleep(1)
+print("\x1b[92mSuccessfully \x1b[31mbricked server\x1b[0m")
+exit()
